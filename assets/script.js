39c0df78528093d7b7bd0e431e5562a74b0405f9
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-02 16:24:39 -0400

    add new array total

--- assets/script.js
+++ assets/script.js
@@ -11,74 +11,76 @@ var generatePassword = function () {
   }
   else {
 
+    var total = []
     var lowerCase = window.confirm("Click 'OK' if you would like to include lowercase letters");
     if (lowerCase) {
       randomPassword.push(randomLetters);
       console.log(randomLetters);
+      total = total.concat(letters)
     }
 
     var upperCase = window.confirm("Click 'OK' if you would like to include uppercase letters");
     if (upperCase) {
       randomPassword.push(randomUpper);
       console.log(randomUpper);
+      total = total.concat(upperLetters)
     }
 
     var symbols = window.confirm("Click 'OK' if you would like to include special characters");
     if (symbols) {
       randomPassword.push(specialChar);
       console.log(specialChar);
+      total = total.concat(specialChar)
     }
 
     var numeric = window.confirm("Click 'OK' if you would like to include numbers");
     if (numeric) {
       randomPassword.push(randomNum);
       console.log(randomNum);
+      total = total.concat(numbers)
     }
-
+  }
     for (var i = 0; i < character; i++) {
 
-      if (randomPassword[i] < character) {
-        randomPassword.push(randomLetters);
-        randomPassword.push(randomUpper);
-        randomPassword.push(specialChar);
-        randomPassword.push(randomNum);
+      if (lowerCase && symbols && numeric && upperCase) {
+        randomPassword.push(total);
       }
-      console.log(randomPassword[i]);
+      console.log(total[i]);
     }
   }
+
+
+
+//array of lowercase letters  
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+//random selection of lowercase letters
+var randomLetters = letters[Math.floor(Math.random() * letters.length)]
+//array of uppercase letters
+var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+//random selection of uppercase letters
+var randomUpper = upperLetters[Math.floor(Math.random() * upperLetters.length)]
+//array of special characters
+var special = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+//random selection of special characters
+var specialChar = special[Math.floor(Math.random() * special.length)]
+//array of numbers
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//random selection of numbers
+var randomNum = parseInt(numbers[Math.floor(Math.random() * numbers.length)])
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
-
-  //array of lowercase letters  
-  var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  //random selection of lowercase letters
-  var randomLetters = letters[Math.floor(Math.random() * letters.length)]
-  //array of uppercase letters
-  var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  //random selection of uppercase letters
-  var randomUpper = upperLetters[Math.floor(Math.random() * upperLetters.length)]
-  //array of special characters
-  var special = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
-  //random selection of special characters
-  var specialChar = special[Math.floor(Math.random() * special.length)]
-  //array of numbers
-  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  //random selection of numbers
-  var randomNum = parseInt(numbers[Math.floor(Math.random() * numbers.length)])
-
-
-
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
